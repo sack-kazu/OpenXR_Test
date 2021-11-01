502f87b646f746377afbe0645418fc265c2a33b7
sack-kazu <sack.kazu@gmail.com>
2021-11-01 15:12:11 +0900

    ・Unityバージョンを2021.2.0f1に ・デバイスの検知を追加

---
+++ Assets/Scripts/InputSystemDeviceDetector.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputSystemDeviceDetector : MonoBehaviour
+{
+    [SerializeField] UnityEngine.UI.Text _text;
+    string _oculus = "Oculus", _valve = "Valve", _htc = "HTC";
+    private void Awake()
+    {
+        InputSystem.onDeviceChange += OnDeviceChange;
+    }
+
+    private void OnApplicationQuit()
+    {
+        InputSystem.onDeviceChange -= OnDeviceChange;
+    }
+    
+    private void OnDeviceChange(InputDevice inputDevice, InputDeviceChange inputDeviceChange)
+    {
+        switch (inputDeviceChange)
+        {
+            case InputDeviceChange.Added:
+                OnDeviceAdded(inputDevice);
+                break;
+        }
+    }
+
+    private void OnDeviceAdded(InputDevice inputDevice)
+    {
+        Debug.Log($"Device Added: {inputDevice.displayName}\nManufacturer: {inputDevice.description.manufacturer}\nProduct: {inputDevice.description.product}");
+        if(inputDevice.description.manufacturer  == _oculus){
+            _text.text = $"This is {_oculus} device.";
+        }else if(inputDevice.description.manufacturer  == _valve){
+            _text.text = $"This is {_valve} device.";
+        }else if(inputDevice.description.manufacturer  == _htc){
+            _text.text = $"This is {_htc} device.";
+        }else{
+            _text.text = $"This is unknown device.";
+        }
+    }
+
+    private void OnDeviceRemoved(InputDevice inputDevice)
+    {
+        Debug.Log($"Device Removed: {inputDevice.displayName}");
+    }
+}
